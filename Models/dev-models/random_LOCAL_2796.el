c1af986ba5a27338d68eecba9b9c8647fdff85ac
Daniel Atanasov <daniel.a.atanasov97@gmail.com>
2019-02-14 18:14:12 +0200

    Removed bullshit form repository

--- Models/dev-models/random_LOCAL_2796.el
+++
@@ -1,5 +0,0 @@
-(defadvice split-window-vertically
-  (after my-window-splitting-advice first () activate)
-  (set-window-buffer (next-window) (other-buffer)))
-  
-;; Fuck you noob
--- Models/dev-models/random_REMOTE_11584.el
+++
@@ -1,3 +0,0 @@
-(defadvice split-window-vertically                     ;; You suck.
-  (after my-window-splitting-advice first () activate) ;; This code sucks.
-  (set-window-buffer (next-window) (other-buffer)))    ;; Thine maternal parent ist homosexual.
