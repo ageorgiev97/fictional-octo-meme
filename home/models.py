2e12126c88f48ca31dd942b2f6d69704dc1e3829
Daniel <daniel.a.atanasov97@gmail.com>
2019-05-01 13:14:30 +0300

    Added proper location string representation

--- home/models.py
+++ home/models.py
@@ -14,6 +14,9 @@ class Location(Model):
     latitude = DecimalField(max_digits=6, decimal_places=3)
     longitude = DecimalField(max_digits=6, decimal_places=3)
 
+    def __str__(self):
+        return f'{self.latitude, self.longitude}'
+
 
 class Restaurant(Model):
     name = CharField(max_length=64)
